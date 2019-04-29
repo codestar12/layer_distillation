8cff3b786c33a7d49dda65f0bb5b5a96d28d30d1
cody <cjb92@txstate.edu>
2019-04-29 16:14:51 -0500

    add plot script

--- plot_log_data.py
+++ plot_log_data.py
@@ -4,6 +4,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import os
+
+#%%
+files = os.listdir('./logs/vgg16_cifar10/')
 
 #%%
 with open('./logs/refactor_log.json', 'r') as f:
@@ -14,34 +18,43 @@ sns.lineplot(x=[layer['layer'] for layer in log['layer']],
 
 #%%
 data_frame_list = []
-for layer in log['layer']:
-    data_frame_list.append((
-        layer['layer'],
-        layer['loss'],
-        layer['acc'],
-        layer['model_loss'],
-        layer['train_time'],
-        log['original_acc'],
-        log['original_loss'],
-        log['train_time'],
-        'layer'
-    ))
-for layer in log['layer']:
-    data_frame_list.append((
-        layer['layer'],
-        layer['loss'],
-        layer['fine_tune_acc'],
-        layer['fine_tune_model_loss'],
-        layer['train_time'],
-        log['original_acc'],
-        log['original_loss'],
-        log['train_time'],
-        'fine_tune'
-    ))
+for file in files:
+    with open('./logs/vgg16_cifar10/' + file) as f:
+        log = json.load(f)
+    for layer in log['layer']:
+        data_frame_list.append((
+            layer['layer'],
+            layer['loss'],
+            log['model_epocss'],
+            log['layer_epochs'],
+            layer['acc'],
+            layer['model_loss'],
+            layer['train_time'],
+            log['original_acc'],
+            log['original_loss'],
+            log['train_time'],
+            'layer'
+        ))
+    for layer in log['layer']:
+        data_frame_list.append((
+            layer['layer'],
+            layer['loss'],
+            log['model_epocss'],
+            log['layer_epochs'],
+            layer['fine_tune_acc'],
+            layer['fine_tune_model_loss'],
+            layer['train_time'],
+            log['original_acc'],
+            log['original_loss'],
+            log['train_time'],
+            'fine_tune'
+        ))
 
 #%%
 columns = ['layer',
            'layer_loss',
+           'fine_tune_epochs',
+           'layer_epochs',
            'acc',
            'model_loss',
            'layer_train_time',
@@ -54,8 +67,11 @@ columns = ['layer',
 df = pd.DataFrame(data_frame_list, columns=columns)
 
 #%%
+top_df = df[df['layer_epochs'] == 4]
+top_df = top_df[top_df['fine_tune_epochs'] == 8]
+#%%
 plt.plot()
-sns.lineplot(x='layer', y='acc', hue='stage', data=df)
+sns.lineplot(x='layer', y='acc', hue='stage', data=top_df)
 plt.hlines(y=log['original_acc'], 
            xmin=1, 
            xmax=12, 
@@ -65,7 +81,34 @@ plt.show()
 
 #%%
 plt.plot()
+sns.lineplot(x='layer', y='model_loss', hue='stage', data=top_df)
+plt.hlines(y=log['original_acc'], 
+           xmin=1, 
+           xmax=12, 
+           colors=['red'],
+           linestyles='dashed')
+plt.show()
+
+
+
+#%%
+plt.plot()
 sns.lineplot(x='layer', y='layer_train_time',  data=df)
 plt.show()
+
+#%%
+new_df = df[df['layer'] == 12]
+new_df = new_df[new_df['stage'] == 'fine_tune']
+#%%
+plt.plot()
+sns.lineplot(x='fine_tune_epochs', y='acc', hue='layer_epochs', data=new_df )
+plt.hlines(y=log['original_acc'], 
+           xmin=1, 
+           xmax=16, 
+           colors=['red'],
+           linestyles='dashed')
+plt.show()
+
+
 
 
